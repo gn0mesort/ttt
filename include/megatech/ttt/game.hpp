6b2094293d37c9fbcd059153b70bf9b0ef0b873e
Alexander Rothman <gnomesort@megate.ch>
2024-03-05 22:47:39 -0800

    Expand the game object.
    This adds a variety of features to megatech::ttt::game. Most
    importantly, this adds new constructors (one for a new game and one for
    loading), a stream insertion operator (for printing to std::ostream
    objects), logic for turn taking, and logic to ensure that lock files are
    unlocked when a game object throws.
    
    Turn taking is probably the most significant addition. Basically, a game
    will proceed by first giving the X (first) player a turn. After X takes
    a turn, the game will proceed to update its state. This proceeds by
    first checking for a win. Next, the game checks to see if the board is
    entirely full. If it is then the game is a draw. Otherwise, the game
    allows the O (second) player to take a turn. After O's turn the state is
    updated the same way.
    
    Single player games are not yet implemented.

--- include/megatech/ttt/game.hpp
+++ include/megatech/ttt/game.hpp
@@ -1,8 +1,10 @@
 #ifndef MEGATECH_TTT_GAME_HPP
 #define MEGATECH_TTT_GAME_HPP
 
+#include <cstddef>
 #include <cinttypes>
 
+#include <iosfwd>
 #include <string>
 #include <filesystem>
 
@@ -18,17 +20,54 @@ namespace megatech::ttt {
   std::string to_string(const game_mode mode);
   game_mode to_game_mode(const std::string& name);
 
+  enum class play_state : std::uint32_t {
+    turn_x = 0,
+    turn_o = 0x10'00'00'00,
+    win_x = 0x20'00'00'00,
+    win_o = 0x30'00'00'00,
+    draw = 0x40'00'00'00
+  };
+
   const std::filesystem::path DEFAULT_GAME_NAME{ ".ttt" };
 
   class game final {
   private:
     static constexpr const std::uint32_t MULTIPLAYER_BIT{ 0x80'00'00'00 };
+    static constexpr const std::uint32_t PLAY_STATE_MASK{ 0x70'00'00'00 };
+    static constexpr const std::uint32_t BOARD_MASK{ 0x00'03'ff'ff };
+    static constexpr const std::uint32_t ALL_CELL_BITS{ 0xff };
+
+    static constexpr const std::uint32_t  BOARD_UPPER_ROW_MASK{ 0x00'00'00'3f };
+    static constexpr const std::uint32_t BOARD_MIDDLE_ROW_MASK{ 0x00'00'0f'c0 };
+    static constexpr const std::uint32_t BOARD_BOTTOM_ROW_MASK{ 0x00'03'f0'00 };
+    static constexpr const std::uint32_t   BOARD_LEFT_COLUMN_MASK{ 0x00'00'30'c3 };
+    static constexpr const std::uint32_t BOARD_CENTER_COLUMN_MASK{ 0x00'00'c3'0c };
+    static constexpr const std::uint32_t  BOARD_RIGHT_COLUMN_MASK{ 0x00'03'0c'30 };
+    static constexpr const std::uint32_t BOARD_LEFT_TO_RIGHT_DIAGONAL_MASK{ 0x00'03'03'03 };
+    static constexpr const std::uint32_t BOARD_RIGHT_TO_LEFT_DIAGONAL_MASK{ 0x00'00'33'30 };
+    static constexpr const std::size_t BOARD_MIDDLE_ROW_SHIFT{ 6 };
+    static constexpr const std::size_t BOARD_BOTTOM_ROW_SHIFT{ 12 };
+    static constexpr const std::uint32_t   BOARD_LEFT_CELL_MASK{ 0x03 };
+    static constexpr const std::uint32_t BOARD_CENTER_CELL_MASK{ 0x0c };
+    static constexpr const std::uint32_t  BOARD_RIGHT_CELL_MASK{ 0x30 };
+    static constexpr const std::size_t BOARD_CENTER_CELL_SHIFT{ 2 };
+    static constexpr const std::size_t  BOARD_RIGHT_CELL_SHIFT{ 4 };
+    static constexpr const std::uint32_t BOARD_CELL_EMPTY{ 0x00 };
+    static constexpr const std::uint32_t     BOARD_CELL_X{ 0x01 };
+    static constexpr const std::uint32_t     BOARD_CELL_O{ 0x02 };
 
     std::uint32_t m_state{ };
     std::filesystem::path m_path{ };
     details::lockfile m_lock{ ".ttt" };
+
+    void read_data_file();
+    bool is_board_full() const;
+    std::uint32_t find_winner() const;
+    void update_play_state();
+    void take_turn(const std::size_t column, const std::size_t row, const std::uint32_t value);
   public:
     game(const std::filesystem::path& path);
+    game(const std::filesystem::path& path, const game_mode mode);
     game(const game& other) = delete;
     game(game&& other) = default;
 
@@ -37,10 +76,53 @@ namespace megatech::ttt {
     game& operator=(const game& rhs) = delete;
     game& operator=(game&& rhs) = delete;
 
-    void clear();
     game_mode get_mode() const;
-    void set_mode(const game_mode mode);
+    play_state get_play_state() const;
+    char get_cell(const std::size_t column, const std::size_t row) const;
+    void take_turn(const std::size_t column, const std::size_t row);
   };
+
+  template <typename CharT, typename Traits>
+  std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, const game& g) {
+    os << "    0  1  2 " << std::endl
+       << "  0  " << g.get_cell(0, 0) << "|" << g.get_cell(1, 0) << "|" << g.get_cell(2, 0) << "  " << std::endl
+       << "    ------- " << std::endl
+       << "  1  " << g.get_cell(0, 1) << "|" << g.get_cell(1, 1) << "|" << g.get_cell(2, 1) << "  " << std::endl
+       << "    ------- " << std::endl
+       << "  2  " << g.get_cell(0, 2) << "|" << g.get_cell(1, 2) << "|" << g.get_cell(2, 2) << "  "<< std::endl;
+    switch (g.get_mode())
+    {
+    case game_mode::single_player:
+      os << "This is a single player game." << std::endl;
+      break;
+    case game_mode::multiplayer:
+      os << "This is a multiplayer game." << std::endl;
+      break;
+    default:
+      break;
+    }
+    switch (g.get_play_state())
+    {
+    case play_state::turn_x:
+      os << "It is X's turn.";
+      break;
+    case play_state::turn_o:
+      os << "It is O's turn.";
+      break;
+    case play_state::win_x:
+      os << "X has won the game.";
+      break;
+    case play_state::win_o:
+      os << "O has won the game.";
+      break;
+    case play_state::draw:
+      os << "The game ended in a Cat's Game.";
+      break;
+    default:
+      break;
+    }
+    return os;
+  }
 
 }
 
--- src/megatech/ttt/game.cpp
+++ src/megatech/ttt/game.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <cassert>
 
+#include <bit>
 #include <stdexcept>
 #include <fstream>
 
@@ -40,57 +41,242 @@ namespace megatech::ttt {
     throw std::runtime_error{ "The input game mode was not recognized." };
   }
 
-  game::game(const std::filesystem::path& path) : m_path{ std::filesystem::absolute(path) },
-                                                  m_lock{ m_path } {
-    m_lock.lock();
-    auto stat = std::filesystem::status(m_path);
-    // Can we even stat the file?
-    if (!std::filesystem::status_known(stat))
+  void game::read_data_file() {
+    auto f_in = std::ifstream{ m_path, std::ios::binary | std::ios::ate };
+    if (f_in.tellg() < 0 || static_cast<std::size_t>(f_in.tellg()) < sizeof(details::data_file_header))
     {
-      throw std::runtime_error{ "The status of the game data file could not be determined." };
+      throw std::runtime_error{ "The requested game data file is too short to be valid." };
     }
-    // If the game data file exists then check if it is regular.
-    if (std::filesystem::exists(stat))
+    f_in.seekg(0, std::ios::beg);
+    auto header = details::data_file_header{ };
+    f_in.read(reinterpret_cast<char*>(&header), sizeof(details::data_file_header));
+    if (std::memcmp(header.magic, details::DATA_FILE_HEADER_MAGIC, details::DATA_FILE_HEADER_MAGIC_LENGTH) != 0 ||
+        header.version != details::DATA_FILE_VERSION_1)
     {
-      // If the game data file is regular then read it back.
-      if (std::filesystem::is_regular_file(stat))
+      throw std::runtime_error{ "The game data file is corrupt." };
+    }
+    auto body = details::data_file_body_v1{ };
+    f_in.read(reinterpret_cast<char*>(&body), sizeof(details::data_file_body_v1));
+    switch (body.endianness)
+    {
+    case details::DATA_FILE_REVERSE_ENDIANNESS:
+      m_state = BSWAP32(body.state);
+      break;
+    case details::DATA_FILE_CORRECT_ENDIANNESS:
+      m_state = body.state;
+      break;
+    default:
+      throw std::runtime_error{ "The game data file is corrupt or it was written with an unknown byte order." };
+    }
+  }
+
+  bool game::is_board_full() const {
+    // There are 9 cells total. Each cell can be in one of three states: 00 (empty), 01 (X), 10 (O).
+    // There is no 11 state.
+    //
+    // Therefore, if the popcount of the board is 9 or more it is full.
+    return std::popcount(m_state & BOARD_MASK) >= 9;
+  }
+
+  std::uint32_t game::find_winner() const {
+    const auto board = m_state & BOARD_MASK;
+    // Check Rows
+    switch (board & BOARD_UPPER_ROW_MASK)
+    {
+    case 0x00'00'00'15:
+      return BOARD_CELL_X;
+    case 0x00'00'00'2a:
+      return BOARD_CELL_O;
+    default:
+      break;
+    }
+    switch (board & BOARD_MIDDLE_ROW_MASK)
+    {
+    case 0x00'00'05'40:
+      return BOARD_CELL_X;
+    case 0x00'00'0a'80:
+      return BOARD_CELL_O;
+    default:
+      break;
+    }
+    switch (board & BOARD_BOTTOM_ROW_MASK)
+    {
+    case 0x00'01'50'00:
+      return BOARD_CELL_X;
+    case 0x00'02'a0'00:
+      return BOARD_CELL_O;
+    default:
+      break;
+    }
+    // Check Columns
+    switch (board & BOARD_LEFT_COLUMN_MASK)
+    {
+    case 0x00'00'10'41:
+      return BOARD_CELL_X;
+    case 0x00'00'20'82:
+      return BOARD_CELL_O;
+    default:
+      break;
+    }
+    switch (board & BOARD_CENTER_COLUMN_MASK)
+    {
+    case 0x00'00'41'04:
+      return BOARD_CELL_X;
+    case 0x00'00'82'08:
+      return BOARD_CELL_O;
+    default:
+      break;
+    }
+    switch (board & BOARD_RIGHT_COLUMN_MASK)
+    {
+    case 0x00'01'04'10:
+      return BOARD_CELL_X;
+    case 0x00'02'08'20:
+      return BOARD_CELL_O;
+    default:
+      break;
+    }
+    // Check Diagonals
+    switch (board & BOARD_LEFT_TO_RIGHT_DIAGONAL_MASK)
+    {
+    case 0x00'01'01'01:
+      return BOARD_CELL_X;
+    case 0x00'02'02'02:
+      return BOARD_CELL_O;
+    default:
+      break;
+    }
+    switch (board & BOARD_RIGHT_TO_LEFT_DIAGONAL_MASK)
+    {
+    case 0x00'00'11'10:
+      return BOARD_CELL_X;
+    case 0x00'00'22'20:
+      return BOARD_CELL_O;
+    default:
+      break;
+    }
+    return BOARD_CELL_EMPTY;
+  }
+
+  void game::update_play_state() {
+    switch (find_winner())
+    {
+    case BOARD_CELL_X:
+      m_state = (m_state & ~PLAY_STATE_MASK) | static_cast<std::uint32_t>(play_state::win_x);
+      return;
+    case BOARD_CELL_O:
+      m_state = (m_state & ~PLAY_STATE_MASK) | static_cast<std::uint32_t>(play_state::win_o);
+      return;
+    default:
+      break;
+    }
+    if (game::is_board_full())
+    {
+      m_state = (m_state & ~PLAY_STATE_MASK) | static_cast<std::uint32_t>(play_state::draw);
+      return;
+    }
+    switch (get_play_state())
+    {
+    case play_state::turn_x:
+      m_state = (m_state & ~PLAY_STATE_MASK) | static_cast<std::uint32_t>(play_state::turn_o);
+      return;
+    case play_state::turn_o:
+      m_state = (m_state & ~PLAY_STATE_MASK) | static_cast<std::uint32_t>(play_state::turn_x);
+      return;
+    default:
+      assert(((void) "Unreachable", false));
+    }
+  }
+
+  void game::take_turn(const std::size_t column, const std::size_t row, const std::uint32_t value) {
+    if (get_cell(column, row) != ' ')
+    {
+      throw std::runtime_error{ "The desired turn was invalid because the cell was already filled." };
+    }
+    auto cell = std::uint32_t{ 0 };
+    switch (column)
+    {
+    case 0:
+      cell = value;
+      break;
+    case 1:
+      cell = value << BOARD_CENTER_CELL_SHIFT;
+      break;
+    case 2:
+      cell = value << BOARD_RIGHT_CELL_SHIFT;
+      break;
+    default:
+      throw std::runtime_error{ "The input column was invalid." };
+    }
+    auto row_bits = std::uint32_t{ 0 };
+    switch (row)
+    {
+    case 0:
+      row_bits = cell;
+      break;
+    case 1:
+      row_bits = cell << BOARD_MIDDLE_ROW_SHIFT;
+      break;
+    case 2:
+      row_bits = cell << BOARD_BOTTOM_ROW_SHIFT;
+      break;
+    default:
+      throw std::runtime_error{ "The input row was invalid." };
+    }
+    m_state |= row_bits;
+    update_play_state();
+  }
+
+  game::game(const std::filesystem::path& path) : m_path{ std::filesystem::absolute(path) }, m_lock{ m_path } {
+    try
+    {
+      m_lock.lock();
+      auto stat = std::filesystem::status(m_path);
+      if (!std::filesystem::status_known(stat))
       {
-        auto f_in = std::ifstream{ m_path, std::ios::binary | std::ios::ate };
-        if (f_in.tellg() < 0 || static_cast<std::size_t>(f_in.tellg()) < sizeof(details::data_file_header))
-        {
-          throw std::runtime_error{ "The requested game data file is too short to be valid." };
-        }
-        f_in.seekg(0, std::ios::beg);
-        auto header = details::data_file_header{ };
-        f_in.read(reinterpret_cast<char*>(&header), sizeof(details::data_file_header));
-        if (std::memcmp(header.magic, details::DATA_FILE_HEADER_MAGIC, details::DATA_FILE_HEADER_MAGIC_LENGTH) != 0 ||
-            header.version != details::DATA_FILE_VERSION_1)
-        {
-          throw std::runtime_error{ "The game data file is corrupt." };
-        }
-        auto body = details::data_file_body_v1{ };
-        f_in.read(reinterpret_cast<char*>(&body), sizeof(details::data_file_body_v1));
-        switch (body.endianness)
-        {
-        case details::DATA_FILE_REVERSE_ENDIANNESS:
-          m_state = BSWAP32(body.state);
-          break;
-        case details::DATA_FILE_CORRECT_ENDIANNESS:
-          m_state = body.state;
-          break;
-        default:
-          throw std::runtime_error{ "The game data file is corrupt or it was written with an unknown byte order." };
-        }
+        throw std::runtime_error{ "The status of the game data file could not be determined." };
       }
-      else
+      if (!std::filesystem::exists(stat))
+      {
+        throw std::runtime_error{ "The requested game data file does not exist." };
+      }
+      if (!std::filesystem::is_regular_file(stat))
       {
         throw std::runtime_error{ "The requested game data file is not a regular file." };
       }
+      read_data_file();
     }
-    // If the game data file does not exist start an empty game.
-    else
+    catch (...)
     {
-      m_state = 0;
+      m_lock.unlock();
+      throw;
+    }
+  }
+
+  game::game(const std::filesystem::path& path, const game_mode mode) : m_path{ std::filesystem::absolute(path) },
+                                                                        m_lock{ m_path } {
+    try
+    {
+      m_lock.lock();
+      auto stat = std::filesystem::status(m_path);
+      if (!std::filesystem::status_known(stat))
+      {
+        throw std::runtime_error{ "The status of the game data file could not be determined." };
+      }
+      if (std::filesystem::exists(stat))
+      {
+        if (!std::filesystem::is_regular_file(stat))
+        {
+          throw std::runtime_error{ "The requested game data file is not a regular file." };
+        }
+        read_data_file();
+      }
+      m_state = 0 | static_cast<std::uint32_t>(mode);
+    }
+    catch (...)
+    {
+      m_lock.unlock();
+      throw;
     }
   }
 
@@ -109,16 +295,69 @@ namespace megatech::ttt {
     m_lock.unlock();
   }
 
-  void game::clear() {
-    m_state = 0;
-  }
-
   game_mode game::get_mode() const {
     return static_cast<game_mode>(m_state & MULTIPLAYER_BIT);
   }
 
-  void game::set_mode(const game_mode m) {
-    m_state = (m_state & ~MULTIPLAYER_BIT) | static_cast<std::uint32_t>(m);
+  play_state game::get_play_state() const {
+    return static_cast<play_state>(m_state & PLAY_STATE_MASK);
+  }
+
+  char game::get_cell(const std::size_t column, const std::size_t row) const {
+    auto row_bits = std::uint32_t{ 0 };
+    switch (row)
+    {
+    case 0:
+      row_bits = m_state & BOARD_UPPER_ROW_MASK;
+      break;
+    case 1:
+      row_bits = (m_state & BOARD_MIDDLE_ROW_MASK) >> BOARD_MIDDLE_ROW_SHIFT;
+      break;
+    case 2:
+      row_bits = (m_state & BOARD_BOTTOM_ROW_MASK) >> BOARD_BOTTOM_ROW_SHIFT;
+      break;
+    default:
+      throw std::runtime_error{ "The input row was invalid." };
+    }
+    auto cell = std::uint32_t{ 0 };
+    switch (column)
+    {
+    case 0:
+      cell = row_bits & BOARD_LEFT_CELL_MASK;
+      break;
+    case 1:
+      cell = (row_bits & BOARD_CENTER_CELL_MASK) >> BOARD_CENTER_CELL_SHIFT;
+      break;
+    case 2:
+      cell = (row_bits & BOARD_RIGHT_CELL_MASK) >> BOARD_RIGHT_CELL_SHIFT;
+      break;
+    default:
+      throw std::runtime_error{ "The input column was invalid." };
+    }
+    switch (cell)
+    {
+    case BOARD_CELL_EMPTY:
+      return ' ';
+    case BOARD_CELL_X:
+      return 'X';
+    case BOARD_CELL_O:
+      return 'O';
+    }
+    throw std::runtime_error{ "The game state was corrupted." };
+  }
+
+  void game::take_turn(const std::size_t column, const std::size_t row) {
+    switch (get_play_state())
+    {
+    case play_state::turn_x:
+      take_turn(column, row, BOARD_CELL_X);
+      break;
+    case play_state::turn_o:
+      take_turn(column, row, BOARD_CELL_O);
+      break;
+    default:
+      break;
+    }
   }
 
 }
