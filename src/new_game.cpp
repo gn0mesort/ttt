c2023f978846f95487c524994315cd7b5a9b90e5
Alexander Rothman <gnomesort@megate.ch>
2024-03-05 22:56:01 -0800

    Update new game application.
    Mostly, this is to handle the new constructor and interface changes for
    megatech::ttt::game. It also adds logic to print the game to standard
    output before exiting.

--- src/new_game.cpp
+++ src/new_game.cpp
@@ -17,7 +17,6 @@ int main(int argc, char** argv) {
               << "for a single player or multiplayer game respectively." << std::endl;
     return 1;
   }
-  auto g = static_cast<megatech::ttt::game*>(nullptr);
   try
   {
     auto mode = megatech::ttt::game_mode::single_player;
@@ -30,32 +29,25 @@ int main(int argc, char** argv) {
       mode = megatech::ttt::to_game_mode(mode_str);
     }
     auto home_dir = megatech::ttt::find_home_directory();
-    std::cerr << "A home directory was found @ " << home_dir << "." << std::endl;
     auto game_path = home_dir / megatech::ttt::DEFAULT_GAME_NAME;
     auto stat = std::filesystem::status(game_path);
     if (std::filesystem::exists(stat))
     {
       std::cerr << "An existing game file was found @ " << game_path
                 << ". An attempt will be made to clear the file." << std::endl;
-      g = new megatech::ttt::game{ game_path };
-      g->clear();
     }
     else
     {
       std::cerr << "No existing game file found @ " << game_path << ". Creating a new game." << std::endl;
-      g = new megatech::ttt::game{ game_path };
     }
-    g->set_mode(mode);
-    delete g;
-    std::cerr << "A new game was successfully created!" << std::endl;
+    {
+      auto g = megatech::ttt::game{ game_path, mode };
+      std::cout << g << std::endl;
+    }
   }
   catch (const std::exception& err)
   {
     std::cerr << "An error occurred:" << std::endl << "\t\"" << err.what() << "\"" << std::endl;
-    if (g)
-    {
-      delete g;
-    }
     return 1;
   }
   return 0;
